d57c4a1ec7d19b2a066f417d989eb81f35f61072
Jiansen <jiansenhe@gmail.com>
2016-06-29 20:49:00 +0100

    feat: function pattern match

--- src/index.js
+++ src/index.js
@@ -1,7 +1,30 @@
-const match = obj => pattern => {
-    if (obj in pattern){
-        return pattern[obj];
+const primitivePatternHandler = (exp, pattern) => {
+  if(pattern[0] === exp) {
+    return pattern[1];
+  }
+  return undefined;
+}
+
+const handlerByPatternType = {
+  'string': primitivePatternHandler,
+  'number': primitivePatternHandler,
+  'function': (exp, pattern) => {
+    if ('unapply' in pattern[0].prototype) {
+      return pattern[1](pattern[0].prototype.unapply(exp));
     }
+    return undefined;
+  }
+}
+
+const match = exp => patterns => {
+  for (const pattern of patterns) {
+    const optionPrimitiveResult = handlerByPatternType[typeof pattern[0]](exp, pattern);
+    if (optionPrimitiveResult != undefined) {
+      return optionPrimitiveResult;
+    }
+  };
+
+  return undefined;
 }
 
 export default match;
--- test/match.spec.es6.js
+++ test/match.spec.es6.js
@@ -5,23 +5,59 @@ chai.expect();
 const expect = chai.expect;
 
 describe('Matching on build-in primitive types', function () {
-  describe('Integer value', function () {
-    it('should match the same value', () => {
-        const m = match(1)({
-            1: "ONE",
-            2: "TWO"
-        })
-        expect(m).to.be.equal('ONE');
+  describe('Integer value 10', function () {
+    it('should match value 10', () => {
+        const m = match(10)([
+            [1, "ONE"],
+            [10, "TEN"],
+        ])
+        expect(m).to.be.equal('TEN');
     });
   });
-  describe('Integer veriable', function () {
-      it('should match the same value', () => {
-          const twoVar = 2;
-          const n = match(twoVar)({
-              1: "ONE",
-              2: "TWO"
-          })
-          expect(n).to.be.equal('TWO');
+  describe('Integer veriable has value 2', function () {
+      it('should match the value 2', () => {
+          const two = 2;
+          const m = match(two)([
+              [1, "ONE"],
+              [2, "TWO"],
+          ])
+          expect(m).to.be.equal('TWO');
       });
+  });
+});
+
+describe('Matching on class contructor', function () {
+  class Twice {
+    constructor(x) {
+      this.value = x*2;
+    }
+    unapply(x) {
+      if( x%2 ==0 ) {
+        return x/2;
+      }else{
+        return undefined;
+      }
+    }
+  }
+  describe('10 ', function () {
+    it('should match Twice(5)', () => {
+        const m = match(10)([
+            [1, "ONE"],
+            [2, "TWO"],
+            [Twice, (x) => x],
+        ])
+        expect(m).to.be.equal(5);
+    });
+  });
+  describe('Twice(5)', function () {
+    it('should match Twice(5)', () => {
+        const twice5 = new Twice(5);
+        const m = match(10)([
+          [1, "ONE"],
+          [2, "TWO"],
+          [Twice, (x) => x],
+        ])
+        expect(m).to.be.equal(5);
+    });
   });
 });
