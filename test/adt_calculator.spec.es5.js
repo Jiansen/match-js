6dbd307b4eb89ba623118f47c4d7cdb064eafd29
jhe <jhe@factset.com>
2016-07-01 15:31:15 +0100

    feat: add es5 tests

---
+++ test/adt_calculator.spec.es5.js
@@ -0,0 +1,85 @@
+var chai = require('chai');
+var match = require('../lib/match.js');
+
+chai.expect();
+var expect = chai.expect;
+
+describe('Matching on Algebraic Data Type: An Calculator', function () {
+  function BinOpExp(x,y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  function Add(x,y) {
+    BinOpExp.call(this, x, y);
+  }
+  Add.prototype = Object.create(BinOpExp.prototype);
+  Add.prototype.unapply = function unapply(exp) {
+    if (exp instanceof Add){
+      return [exp.x, exp.y];
+    }
+    return undefined;
+  }
+
+  function Min(x,y) {
+    BinOpExp.call(this, x, y);
+  }
+  Min.prototype = Object.create(BinOpExp.prototype);
+  Min.prototype.unapply = function unapply(exp) {
+    if (exp instanceof Min){
+      return [exp.x, exp.y];
+    }
+    return undefined;
+  }
+
+  function Mul(x,y) {
+    BinOpExp.call(this, x, y);
+  }
+  Mul.prototype = Object.create(BinOpExp.prototype);
+  Mul.prototype.unapply = function unapply(exp) {
+    if (exp instanceof Mul){
+      return [exp.x, exp.y];
+    }
+    return undefined;
+  }
+
+  function Div(x,y) {
+    BinOpExp.call(this, x, y);
+  }
+  Div.prototype = Object.create(BinOpExp.prototype);
+  Div.prototype.unapply = function unapply(exp) {
+    if (exp instanceof Div){
+      return [exp.x, exp.y];
+    }
+    return undefined;
+  }
+
+  function Number() {}
+  Number.prototype.unapply = function unapply(e) {
+    if (typeof e === 'number'){
+      return e;
+    }
+    return undefined;
+  }
+
+  var evaluate = function evaluate(exp) {
+    return match(exp)([
+      [Add, function(arr) { return evaluate(arr[0])+evaluate(arr[1]); }],
+      [Min, function(arr) { return evaluate(arr[0])-evaluate(arr[1]); }],
+      [Mul, function(arr) { return evaluate(arr[0])*evaluate(arr[1]); }],
+      [Div, function(arr) { return evaluate(arr[0])/evaluate(arr[1]); }],
+      [Number, function(x) { return x}],
+    ])
+  };
+
+  it('evaluate(5) should eval to 5', function() {
+      expect(evaluate(5)).to.be.equal(5);
+  });
+
+  it('/( *( +(3)(2) )(6) ) (2) should eval to 15', function () {
+      var exp = new Div(
+                    new Mul( new Add(3, 2), 6),
+                    2);
+      expect(evaluate(exp)).to.be.equal(15);
+  });
+});
---
+++ test/twice_class.spec.es5.js
@@ -0,0 +1,93 @@
+/*
+Ported From the Scala Example in:
+  http://docs.scala-lang.org/tutorials/tour/extractor-objects.html
+
+
+```
+object Twice {
+  def apply(x: Int): Int = x * 2
+  def unapply(z: Int): Option[Int] = if (z%2 == 0) Some(z/2) else None
+}
+object TwiceTest extends App {
+  val x = Twice(21)
+  x match { case Twice(n) => Console.println(n) } // prints 21
+}
+``
+*/
+
+var chai = require('chai');
+var match = require('../lib/match.js');
+
+chai.expect();
+var expect = chai.expect;
+
+describe('Matching on class contructor Twice:', function () {
+  function Twice(x) {
+    this.value = x*2;
+    this.funny_prop = 'funny';
+  }
+  Twice.prototype.unapply = function unapply(x) {
+    if (x instanceof Twice && x.value%2 ==0) {
+      return x.value / 2
+    }
+
+    if( x%2 ==0 ) {
+      return x/2;
+    }else{
+      return undefined;
+    }
+  }
+
+  it('10 should match Twice(x), where x is assigned to 5', function() {
+    var m = match(10)([
+      [1, "ONE"],
+      [2, "TWO"],
+      [Twice, function(x) {return x;}],
+    ])
+    expect(m).to.be.equal(5);
+  });
+  it('10 should match Twice(5)', function() {
+    var m = match(10)([
+      [1, "ONE"],
+      [2, "TWO"],
+      [new Twice(5), "Twice(5)"],
+    ])
+    expect(m).to.be.equal("Twice(5)");
+  });
+  it('Twice(5) should match Twice(x), where x is assigned to 5', function() {
+    var twice5 = new Twice(5);
+    var m = match(twice5)([
+      [1, "ONE"],
+      [2, "TWO"],
+      [Twice, function(x) {return x;}],
+    ])
+    expect(m).to.be.equal(5);
+  });
+  it('Twice(5) should match new Twice(5), before match with Twice(x)', function() {
+    var twice5 = new Twice(5);
+    var m = match(twice5)([
+      [1, "ONE"],
+      [new Twice(5), "Twice(5)"],
+      [Twice, function(x) {return x;}],
+    ])
+    expect(m).to.be.equal("Twice(5)");
+  });
+  it('Twice(5) should not match Twice(6), but Twice(x)', function() {
+    var twice5 = new Twice(5);
+    var m = match(twice5)([
+      [1, "ONE"],
+      [new Twice(6), "Twice(6)"],
+      [Twice, function(x) {return x;}],
+    ])
+    expect(m).to.be.equal(5);
+  });
+  it('Twice(6) should match {value: 12}', function() {
+    var twice6 = new Twice(6);
+    var m = match(twice6)([
+      [1, "ONE"],
+      [{value: 12}, "Twice(6) matches {value:12}"],
+      [Twice, function(x) {return x;}],
+    ])
+    expect(m).to.be.equal("Twice(6) matches {value:12}");
+  });
+});
