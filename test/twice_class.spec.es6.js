a06926498e75482d083944c35ca1b880404e784c
jhe <jhe@factset.com>
2016-07-01 14:22:21 +0100

    doc: give reference to Twice example

--- test/twice_class.spec.es6.js
+++ test/twice_class.spec.es6.js
@@ -1,3 +1,20 @@
+/*
+Ported From the Scala Example in:
+  http://docs.scala-lang.org/tutorials/tour/extractor-objects.html
+
+
+```
+object Twice {
+  def apply(x: Int): Int = x * 2
+  def unapply(z: Int): Option[Int] = if (z%2 == 0) Some(z/2) else None
+}
+object TwiceTest extends App {
+  val x = Twice(21)
+  x match { case Twice(n) => Console.println(n) } // prints 21
+}
+``
+*/
+
 import chai from 'chai';
 import match from '../lib/match.js';
 
